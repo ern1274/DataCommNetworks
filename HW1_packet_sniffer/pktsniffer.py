78ab8de79d7ddb84dae71fd634dd2b9fbdaaf849
Ethan Nunez <ern1274@rit.edu>
2025-01-31 12:16:29 -0500

    Added argparser and swapped rdpcap to sniff to allow for filters to be applied

--- HW1_packet_sniffer/pktsniffer.py
+++ HW1_packet_sniffer/pktsniffer.py
@@ -1,9 +1,7 @@
 from scapy.all import *
-from scapy.utils import RawPcapReader
-from scapy.layers.l2 import Ether
-from scapy.layers.inet import IP
+import argparse
 
-cap_file_name = "cap_packets/pkt1.pcapng"
+cap_folder_name = "cap_packets/"
 
 def extract_headers(pkt):
     headers = []
@@ -16,12 +14,40 @@ def extract_headers(pkt):
     return headers
 
 def main():
-    cap_file = rdpcap(cap_file_name)
+    parser = argparse.ArgumentParser(description="Open Packet Capture File, "
+                                                 "filter packets and Display"
+                                                 " Packet Headers")
+    parser.add_argument("file_name",
+                        help="Captured Packet File name")
+    parser.add_argument("-host",
+                        help="To show packets from a specific host")
 
-    i = 1
+    parser.add_argument("-port",
+                        help="To show packets from a specific port")
+
+    parser.add_argument("-ip",
+                        help="To show packets from a specific IP address")
+
+    parser.add_argument("-net",
+                        help="To show packets with destination and "
+                             "source IP address")
+
+    parser.add_argument("-c", help="Number of packets to show")
+
+    parser.add_argument("-protocol",
+                        help= "Show packets using a specific protocol",
+                        choices=['tcp','udp','icmp'])
+
+    args = parser.parse_args()
+
+
+    cap_file_name = cap_folder_name + args.file_name
+    cap_file = sniff(offline=cap_file_name)
+    pkt_num = 1
+
     for pkt in cap_file:
-        print(i)
-        i += 1
+        print(pkt_num)
+        pkt_num += 1
         headers = extract_headers(pkt)
         ether_header= headers[0]
         ip_header = headers[1]
@@ -32,6 +58,8 @@ def main():
         print(ip_header)
         print("Protocol: ")
         print(protocol_header)
+
+
 
 
 
