4021d91dc09221af80f42d0f7297638390faf425
Ethan Nunez <ern1274@rit.edu>
2025-02-03 12:24:21 -0500

    Modified argparser to make type and protocol mutually exclusive to avoid contradicting protocol or type filters

--- HW1_packet_sniffer/pktsniffer.py
+++ HW1_packet_sniffer/pktsniffer.py
@@ -1,7 +1,20 @@
 from scapy.all import *
 import argparse
+import os.path
+#from pathlib import Path
+from HW1_packet_sniffer import config
 
-cap_folder_name = "cap_packets/"
+# This is the absolute path to the cap_packets folder
+# This is needed due to a bug where optional arguments
+# if added rendered the program incapable of finding
+# the cap_packets folder, absolute path prevents this
+cap_folder_name = config.cap_folder_name
+
+def file_exists(file_path):
+    file_path = os.path.join(cap_folder_name, file_path)
+    if not os.path.exists(file_path):
+        raise argparse.ArgumentTypeError(f"'{file_path}' does not exist")
+    return file_path
 
 def extract_headers(pkt):
     headers = []
@@ -14,36 +27,45 @@ def extract_headers(pkt):
     return headers
 
 def main():
+
     parser = argparse.ArgumentParser(description="Open Packet Capture File, "
                                                  "filter packets and Display"
                                                  " Packet Headers")
     parser.add_argument("file_name",
-                        help="Captured Packet File name")
-    parser.add_argument("-host",
-                        help="To show packets from a specific host")
+                        help="Captured Packet File name",
+                        type=file_exists)
+    parser.add_argument("-c", help="Number of packets to show",
+                        type=int)
 
-    parser.add_argument("-port",
-                        help="To show packets from a specific port")
+    type_group = parser.add_argument_group("Type",
+                              "Mutually Exclusive types to filter packets by")
+    target_type = type_group.add_mutually_exclusive_group()
+    # "Show packets using a specific host, port or net address"
+    target_type.add_argument("-host",
+                             help="Show packets using a specific host")
+    target_type.add_argument("-port",
+                             help="Show packets using a specific port")
+    target_type.add_argument("-net",
+                             help="Show packets using a specific net address")
 
-    parser.add_argument("-ip",
-                        help="To show packets from a specific IP address")
-
-    parser.add_argument("-net",
-                        help="To show packets with destination and "
-                             "source IP address")
-
-    parser.add_argument("-c", help="Number of packets to show")
-
-    parser.add_argument("-protocol",
-                        help= "Show packets using a specific protocol",
-                        choices=['tcp','udp','icmp'])
+    proto_group = parser.add_argument_group("Protocol",
+                                           "Mutually Exclusive protocols "
+                                           "to filter packets by")
+    protocol = proto_group.add_mutually_exclusive_group()
+    protocol.add_argument("-ip",
+                          help= "IPV4 or IPV6 address")
+    protocol.add_argument("-tcp")
+    protocol.add_argument("-udp")
+    protocol.add_argument("-icmp")
 
     args = parser.parse_args()
 
+    #for arg in vars(args):
+    #    print(arg, " " , getattr(args, arg))
 
-    cap_file_name = cap_folder_name + args.file_name
-    cap_file = sniff(offline=cap_file_name)
+    cap_file = sniff(offline=args.file_name)
     pkt_num = 1
+
 
     for pkt in cap_file:
         print(pkt_num)
@@ -58,6 +80,7 @@ def main():
         print(ip_header)
         print("Protocol: ")
         print(protocol_header)
+
 
 
 
