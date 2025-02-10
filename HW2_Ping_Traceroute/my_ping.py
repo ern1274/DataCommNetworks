596cf9fd16d32472341bfb6d3c3907f23f74c699
Ethan Nunez <ern1274@rit.edu>
2025-02-10 12:56:29 -0500

    Setup homework 2

---
+++ HW2_Ping_Traceroute/my_ping.py
@@ -0,0 +1,18 @@
+import argparse
+
+def setup_parser():
+    """Sets up the argument parser for the ping program
+
+    :return: parser, an initialized ArgumentParser object
+    :rtype: ArgumentParser
+    """
+    parser = argparse.ArgumentParser(description="Open Packet Capture File, "
+                                                 "filter packets and Display"
+                                                 " Packet Headers")
+    parser.add_argument("-c", help="Number of packets to send/receive",
+                        type=int)
+    return parser
+
+def main():
+    parser = setup_parser()
+    args = parser.parse_args()
